661ed27d0e3c4da6c385c065a73a0feac3893dd6
Claudio Santos <claudiogsssantos@gmail.com>
2023-04-16 15:29:07 -0300

    updale

---
+++ repetições/js/script.js
@@ -0,0 +1,23 @@
+let c = 1
+do {
+    console.log('condicionais e laços de repetição')
+    c++
+} while (c <= 53)
+
+/*
+
+let c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
