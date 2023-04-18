d1ff7d47b3dcd2ba924033bedff74b470a1056c3
Claudio Santos <claudiogsssantos@gmail.com>
2023-04-18 20:55:32 -0300

    Update Js

--- repetições/js/script.js
+++ repetições/js/script.js
@@ -1,10 +1,10 @@
+/*
 let c = 1
 do {
     console.log('condicionais e laços de repetição')
     c++
 } while (c <= 53)
 
-/*
 
 let c = 1
 while (c <= 6) {
@@ -20,4 +20,16 @@ console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
+
+var c=1
+while(c<=5){
+  console.log(c);
+  c++
+};
+
 */
+console.log('Vai começar!')
+for(var c = 1;c <= 5; c++){
+  console.log(c)
+}
+console.log('FIm!')
