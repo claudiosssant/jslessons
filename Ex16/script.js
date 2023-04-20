68995f2fecd9da02092b546c93fed99572ee553e
Claudio Santos <claudiogsssantos@gmail.com>
2023-04-19 22:35:37 -0300

    Update

---
+++ Ex16/script.js
@@ -0,0 +1,9 @@
+var inicio = document.getElementsByName('inicio');
+var fim = document.getElementsByName('fim');
+var passo = document.getElementsByName('passo');
+var res = document.querySelector('div#res');
+
+
+function contar() {
+
+}
