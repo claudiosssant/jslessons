e8b37a1e583a4c793f549f8f5fcf6337d35f32f3
Claudio Santos <claudiogsssantos@gmail.com>
2023-04-22 18:18:36 -0300

    update Project piano

---
+++ Projeto piano/main.js
@@ -0,0 +1,18 @@
+// get all keys
+const keys = document.querySelectorAll('.keys');
+
+// play notes
+function playNote(event) {
+   // Key code
+    
+   //Typed or pressed key
+
+   //if key exists
+
+   //play audio
+}
+
+//click with mouse
+
+//keyboard type
+window.addEventListener("keydown", playNote)
