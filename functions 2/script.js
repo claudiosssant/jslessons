54323bd1baa54c6d2e954fec17302b059bda4174
Claudio Santos <claudiogsssantos@gmail.com>
2023-04-28 19:48:42 -0300

    update

---
+++ functions 2/script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.btn')
+
+function createPhrases() {
+    console.log('bora jogar')
+    document.body.style.backgroundColor = 'blue';
+    document.body.innerHTML ="DALE DAOCU" 
+    alert('bora de lol')
+    document.createElement('img');
+    im
+}
+btn.addEventListener('click', createPhrases);
