a62a0e4ce238bb917a3de52d64c9d80bcbbf36fa
Claudio Santos <claudiogsssantos@gmail.com>
2023-05-02 20:34:40 -0300

    Update

---
+++ Loops/script.js
@@ -0,0 +1,19 @@
+//while = enquanto
+
+
+var x = 0;
+
+while(x < 10) {
+    document.write('<br> O valor do x é: ' + x);
+
+    // aumentando o valor do x
+    x++;
+}
+
+
+// for = para
+
+for(a = 0; a <= 20; a++) {
+    // o que tiver aqui dentro
+    document.write('<br> valor do a é: ' + a);
+}
