24458d393fb44bfaf1b53ee711b25ec77d139bee
Claudio Santos <claudiogsssantos@gmail.com>
2023-05-04 21:19:22 -0300

    update

---
+++ map, filter e reduce/script.js
@@ -0,0 +1,35 @@
+
+const produtos = [
+    { id: 1, nome: 'detergente', valor: 2.00, categoria: 'limpeza'},
+    { id: 2, nome: 'amaciante', valor: 6.50, categoria: 'limpeza'},
+    { id: 3, nome: 'pão', valor: 2.00, categoria: 'alimento'},
+    { id: 4, nome: 'queijo', valor: 7.00, categoria: 'alimento'},
+    { id: 5, nome: 'leite', valor: 2.20, categoria: 'alimento'},
+    { id: 6, nome: 'sabonete', valor: 1.20, categoria: 'limpeza'},
+];
+
+// array map
+
+
+//para usar o .map temos que passar uma função callback
+const ids = produtos.map(produto => produto.id);
+const categoria = produtos.map(produto => produto.categoria);
+
+console.log(ids)
+console.log(categoria)
+
+
+//array filter
+
+
+const alimentos = produtos.filter(p => p.categoria === 'alimento');
+
+console.log(alimentos)
+
+
+
+//array reduce 
+
+
+
+
