f6e3f5056a14de563d5bbf49fcdb8eea69837ed6
Claudio Santos <claudiogsssantos@gmail.com>
2023-05-26 20:45:22 -0300

    Update cronometro

--- Cronometro/js/script.js
+++ Cronometro/js/script.js
@@ -1,1 +1,12 @@
-alert('Cronometro')
+const minutesEl = document.querySelector('#minutes')
+const secondsEl = document.querySelector('#seconds')
+const millisecondsEl = document.querySelector('#milliseconds')
+const startBtn = document.querySelector('#startBtn')
+const pauseBtn = document.querySelector('#pauseBtn')
+const resetBtn = document.querySelector('#resetBtn')
+const resumeBtn = document.querySelector('#resumeBtn')
+
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let isPaused = false;
